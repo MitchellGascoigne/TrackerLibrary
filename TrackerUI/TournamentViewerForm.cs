6a78a6ff8c865f287f26d8d6748dbe0515f86ca4
MitchellGascoigne <107447804+MitchellGascoigne@users.noreply.github.com>
2023-04-05 11:43:59 +1000

    TrackerUI
    -Added Common Controls to Tracker UI

--- TrackerUI/TournamentViewerForm.cs
+++ TrackerUI/TournamentViewerForm.cs
@@ -11,5 +11,20 @@ namespace TrackerUI
         {
 
         }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
     }
 }
